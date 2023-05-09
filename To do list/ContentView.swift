8f2e47bcf9aaecb9e51d3b60e91b74fef7ecc0a0
Kim Hayeon <54922913+kimhayeon00@users.noreply.github.com>
2023-05-10 00:49:06 +0900

    Initial Commit

---
+++ To do list/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To do list
+//
+//  Created by 김하연 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To do list
+//
+//  Created by 김하연 on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
