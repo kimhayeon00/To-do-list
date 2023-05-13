3c963f077125cb9f3361b509f76ada25326a7d8b
Kim Hayeon <54922913+kimhayeon00@users.noreply.github.com>
2023-05-13 20:15:14 +0900

    완료한 할 일을 누르면 줄을 그어주는 기능 추가

--- To do list/ContentView.swift
+++ To do list/ContentView.swift
@@ -10,46 +10,53 @@ import Combine
 
 struct ContentView: View {
     @ObservedObject var taskStore = TaskStore()
-    @State var newnewTask : String = ""
-    var searchBar : some View{
-        HStack{
-            TextField("Insert New Task", text : self.$newnewTask)
-            Button(action: self.addnewnewTask, label: {Text("Add New")})
+    @State var newnewTask: String = ""
+    
+    var searchBar: some View {
+        HStack {
+            TextField("Insert New Task", text: self.$newnewTask)
+            Button(action: self.addnewnewTask) {
+                Text("Add New")
+            }
         }
     }
     
-    func addnewnewTask(){
-        taskStore.tasks.append(Task(id:String(taskStore.tasks.count + 1), newTask : newnewTask))
+    func addnewnewTask() {
+        taskStore.tasks.append(Task(id: String(taskStore.tasks.count + 1), newTask: newnewTask))
         self.newnewTask = ""
     }
     
     var body: some View {
-        NavigationView{
-            VStack{
-                searchBar.padding()
-                List{
-                    ForEach(self.taskStore.tasks){
-                            task in
+            NavigationView {
+                VStack {
+                    searchBar.padding()
+                    List {
+                        ForEach(self.taskStore.tasks) { task in
                             Text(task.newTask)
-                        
-                    }.onMove(perform: self.move)
+                                .strikethrough(task.completed)
+                                .onTapGesture {
+                                    taskStore.toggleTaskCompletion(task) // 할 일의 완료 여부 변경
+                                }
+                        }
+                        .onMove(perform: self.move)
                         .onDelete(perform: self.delete)
-                }.navigationTitle("To Do List")
-            }.navigationBarItems(trailing: EditButton())
-        
+                    }
+                    .navigationTitle("To Do List")
+                }
+                .navigationBarItems(trailing: EditButton())
+            }
+        }
+    func move(from source: IndexSet, to destination: Int) {
+        taskStore.tasks.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func delete(at offsets: IndexSet) {
+        taskStore.tasks.remove(atOffsets: offsets)
     }
 }
 
-    
-    func move (from source : IndexSet, to destination : Int){
-        taskStore.tasks.move(fromOffsets: source, toOffset: destination)
-    }
-    func delete (at offsets : IndexSet){
-        taskStore.tasks.remove(atOffsets: offsets)
-    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
 }
--- datastore.swift
+++ datastore.swift
@@ -9,13 +9,30 @@ import Foundation
 import SwiftUI
 import Combine
 
-struct Task : Identifiable {
-    var id = String()
-    var newTask = String()
-    
+//struct Task : Identifiable {
+//    var id = String()
+//    var newTask = String()
+//
+//}
+struct Task: Identifiable {
+    let id: String
+    let newTask: String
+    var completed: Bool // 완료 여부를 표시하기 위한 속성
+
+    init(id: String, newTask: String, completed: Bool = false) {
+        self.id = id
+        self.newTask = newTask
+        self.completed = completed
+    }
 }
 
-class TaskStore : ObservableObject {
-    @Published var tasks = [Task] ()
+class TaskStore: ObservableObject {
+    @Published var tasks = [Task]()
+    
+    func toggleTaskCompletion(_ task: Task) {
+        if let index = tasks.firstIndex(where: { $0.id == task.id }) {
+            tasks[index].completed.toggle()
+        }
+    }
 }
 
